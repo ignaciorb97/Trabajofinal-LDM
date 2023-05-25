72bdf1a176683fcd0d4fd44a53aa723d3e5cbb77
DAM <ignaciorodriguezblanco97@gmail.com>
2023-05-25 14:28:13 +0200

    entrega de trabajo final completo

--- script.js
+++ script.js
@@ -1,16 +1,73 @@
-var darkModeButton = document.getElementById("dark-mode-button");
+// script.js
 
-darkModeButton.addEventListener("click", function() {
-  document.body.classList.toggle("dark-mode");
-  if (document.body.classList.contains("dark-mode")) {
-    darkModeButton.textContent = "Modo Diurno";
-  } else {
-    darkModeButton.textContent = "Modo Nocturno";
-  }
-});
 
-alert("Ignacio Rodriguez Blanco 1ºDAM,anime");
-function mostrarAlerta(){
-  alert("Ignacio Rodriguez Blanco 1ºDAM,anime");
+function cambiarModo() {
+    if (document.body.classList.contains('modo-noche')) {
+        document.body.classList.remove('modo-noche');
+        localStorage.setItem('modo', 'dia');
+    } else {
+        document.body.classList.add('modo-noche');
+        localStorage.setItem('modo', 'noche');
+    }
+}
+
+
+// Obtener el elemento de entrada de texto
+const textoInput = document.getElementById('texto-input');
+
+
+// Obtener el elemento para mostrar el texto
+const textoMostrado = document.getElementById('texto-mostrado');
+
+
+
+
+function abrirModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block';
+}
+
+
+function cerrarModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
+
+
+
+function mostrarTextoAlmacenado() {
+    var textoAlmacenado = sessionStorage.getItem('texto') || 'sin identificar';
+    var textoMostrado = document.getElementById('texto-mostrado');
+    textoMostrado.textContent = textoAlmacenado;
+    
+    
+}
+
+function almacenarEnSessionStorage() {
+    var textoInput = document.getElementById('texto-input').value;
+    sessionStorage.setItem('texto', textoInput);
+    alert('Bienvenido '+textoInput);
+    
+}
+function cambiarFuente(){
+    listValue=localStorage.getItem('fuente');
+    document.body.style.fontFamily = listValue;
+}
+
+// Función para abrir la ventana modal
+function abrirModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block';
+}
+
+// Función para cerrar la ventana modal
+function cerrarModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+function decirHola(){
+    var persona =sessionStorage.getItem('texto');
+    console.log('hola '+persona);
 }
 
